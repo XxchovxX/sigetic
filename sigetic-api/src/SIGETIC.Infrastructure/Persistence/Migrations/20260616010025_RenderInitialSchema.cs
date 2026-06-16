5ed54967a8a2c8a2b40b0bd7146ebd4898e404e1
Cristian Ovalle <cistianovalle@gmail.com>
2026-06-15 20:00:46 -0500

    Agrega migracion inicial para Render

---
+++ sigetic-api/src/SIGETIC.Infrastructure/Persistence/Migrations/20260616010025_RenderInitialSchema.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SIGETIC.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenderInitialSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "roles",
+                keyColumn: "id",
+                keyValue: new Guid("66666666-6666-6666-6666-666666666666"),
+                column: "descripcion",
+                value: "Apoyo administrativo de la Secretaría Administrativa Financiera para consumibles, reportes y trazabilidad.");
+
+            migrationBuilder.UpdateData(
+                table: "roles",
+                keyColumn: "id",
+                keyValue: new Guid("77777777-7777-7777-7777-777777777777"),
+                column: "descripcion",
+                value: "Acceso directivo de consulta, reportes, analítica, tickets y actualización de consumibles.");
+
+            migrationBuilder.UpdateData(
+                table: "usuarios",
+                keyColumn: "id",
+                keyValue: new Guid("99999999-9999-9999-9999-999999999903"),
+                column: "nombre_completo",
+                value: "Auxiliar Administrativo - Secretaría Administrativa Financiera");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "roles",
+                keyColumn: "id",
+                keyValue: new Guid("66666666-6666-6666-6666-666666666666"),
+                column: "descripcion",
+                value: "Apoyo administrativo de la Secretaria Administrativa Financiera para consumibles, reportes y trazabilidad.");
+
+            migrationBuilder.UpdateData(
+                table: "roles",
+                keyColumn: "id",
+                keyValue: new Guid("77777777-7777-7777-7777-777777777777"),
+                column: "descripcion",
+                value: "Acceso directivo de consulta, reportes, analitica, tickets y actualizacion de consumibles.");
+
+            migrationBuilder.UpdateData(
+                table: "usuarios",
+                keyColumn: "id",
+                keyValue: new Guid("99999999-9999-9999-9999-999999999903"),
+                column: "nombre_completo",
+                value: "Auxiliar Administrativo - Secretaria Administrativa Financiera");
+        }
+    }
+}
